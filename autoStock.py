1444616e71ac5716e55afc08a62f8652b11a99fd
Yeh <phil.yeh@kiwa.com>
2024-04-02 21:03:56 +0800

    first commit

--- autoStock.py
+++ autoStock.py
@@ -48,11 +48,11 @@ api.login(api_key="H6WLFjAN6QaRKucnGBm9TgEGpghBNNhafRyj3xjMLp7M",
           fetch_contract=False)
 
 api.fetch_contracts(contract_download=True)
-# print(api.Contracts)
-contracts = [api.Contracts.Futures.MXF['MXF202310']]
+print(api.Contracts.Futures.MXF)
+contracts = [api.Contracts.Futures.MXF.MXFR1]
 today = time.strftime('%Y-%m-%d')
 # print(today)
-symbol = "TXFF"
+#symbol = "TXFF"
 enddate = today
 
 # def quote_callback(exchange:Exchange, tick:TickFOPv1):
@@ -70,53 +70,99 @@ LastTI = 0
 
 while True:
     inTI = time.time()
-    # for i in range(0, 12):
-    # price = dftick['amount'].tail(1).values/dftick['volume'].tail(1).values
-    # print(price)
-    snapshots = api.snapshots(contracts)
-    if inTI - LastTI >= 60:
+    if inTI - LastTI >= 1:
+        # for i in range(0, 12):
+        # price = dftick['amount'].tail(1).values/dftick['volume'].tail(1).values
+        # print(price)
+        snapshots = api.snapshots(contracts)
         kbars = api.kbars(
-            contract=api.Contracts.Futures.MXF['MXF202310'],
+            contract=api.Contracts.Futures.MXF.MXFR1,
             start=startdate, end=enddate
         )
         # 將Tick數據轉換為DataFrame
         df = pd.DataFrame({**kbars})
         df.ts = pd.to_datetime(df.ts)
         df.head(20)
+        df['rsi'] = talib.RSI(df['Close'], timeperiod=14)
+        lastRSI = df['rsi'].tail(1).values[-1]
+        if (lastRSI < 20.0):
+            if (lastRSI > 20.0):
+                buyU = 1
+            else:
+                buyU = 0
+        else:
+            buyU = 0
 
-        df['MA'] = talib.SMA(df['Close'], timeperiod=18)
-        df['MACD'], df['Signal_Line'], _ = talib.MACD(df['Close'], fastperiod=12, slowperiod=26, signalperiod=12)
-        df['STD'] = talib.STDDEV(df['Close'], timeperiod=18)
-        df['Upper'] = df['MA'] + 2 * df['STD']
-        df['Lower'] = df['MA'] - 2 * df['STD']
-        df['K'], df['D'] = talib.STOCH(df['High'], df['Low'], df['Close'], fastk_period=5, slowk_period=3, slowd_period=3)
-        df['J'] = 3 * df['K'] - 2 * df['D']
-        # if (df['K'].tail(1).values > 20) & (df['Close'].tail(1).values > df['Lower'].tail(1).values):
+        if (lastRSI > 80.0):
+            if (lastRSI < 80.0):
+                sellD = 1
+            else:
+                sellD = 0
+        else:
+            sellD = 0
 
-        if (df['K'].tail(1).values < df['D'].tail(1).values) & (df['J'].tail(1).values > 0) & (Buystock == 0) & (df['Close'].tail(1).values < df['Lower'].tail(1).values) &(df['MACD'].tail(1).values > df['Signal_Line'].tail(1).values):
-            buyU = 2
-        elif (df['K'].tail(1).values > df['D'].tail(1).values) & (df['J'].tail(1).values < 0) & (df['Close'].tail(1).values > df['MA'].tail(1).values) &(df['MACD'].tail(1).values < df['Signal_Line'].tail(1).values):
-            sellU = 2
-
-        if (df['K'].tail(1).values > df['D'].tail(1).values) & (df['J'].tail(1).values < 0) & (Sellstock == 0) & (df['Close'].tail(1).values > df['MA'].tail(1).values) &(df['MACD'].tail(1).values < df['Signal_Line'].tail(1).values):
-            sellD = 2
-        if (df['K'].tail(1).values < df['D'].tail(1).values) & (df['J'].tail(1).values > 0) & (df['Close'].tail(1).values < df['Lower'].tail(1).values) &(df['MACD'].tail(1).values > df['Signal_Line'].tail(1).values):
-            buyD = 2
-        # df['Signal'] = df['BuySignal'] + df['SellSignal']
-
-
-        # signal = row['Signal']
-        # close = row['Close']
-        # print(signal, close)
-        if buyU == 2 and Buystock == 0:
+        if buyU == 1 and Buystock == 0:
             now = datetime.now()
             current_time = now.strftime("%y%m%d %H:%M:%S")
             print("Current Time =", current_time, "B ")
+            BuyPoint = snapshots[0].close
+            CheckPointB = snapshots[0].close
             Buystock += 1
+            buyU == 1
+            message = '%s %s %s' % (current_time, "B ", BuyPoint)
+            lineNotifyMessage(token, message)
+        elif sellD == 1 and Buystock == 0:
+            now = datetime.now()
+            current_time = now.strftime("%y%m%d %H:%M:%S")
+            print("Current Time =", current_time, "S ")
+            SellPoint = snapshots[0].close
+            CheckPointD = snapshots[0].close
+            message = '%s %s %s' % (current_time, "S ", SellPoint)
+            lineNotifyMessage(token, message)
+            Buystock += 1
+            sellD == 1
+
+        if (buyU == 1 and Buystock != 0 and (snapshots[0].close - CheckPointB) >= 5):
+            now = datetime.now()
+            current_time = now.strftime("%y%m%d %H:%M:%S")
+            print("Current Time =", current_time, "B ")
+            CheckPointB = snapshots[0].close
+            Buystock += 1
+            message = '%s %s %s' % (current_time, "B ", BuyPoint, CheckPointB)
+            lineNotifyMessage(token, message)
+            buyU = 1
+        elif (buyU == 1 and Buystock > 0 and (CheckPointB - snapshots[0].close) > 0 and (CheckPointB - snapshots[0].close) < 5):
+            now = datetime.now()
+            current_time = now.strftime("%y%m%d %H:%M:%S")
+            print("Current Time =", current_time, "S ")
+            earn =
+            message = '%s %s %s' % (current_time, "S ", snapshots[0].close)
+            buyU = 0
+            lineNotifyMessage(token, message)
+            Buystock = 0
+        if (sellD == 1 and Buystock != 0 and (CheckPointD - snapshots[0].close) > 5):
+            now = datetime.now()
+            current_time = now.strftime("%y%m%d %H:%M:%S")
+            print("Current Time =", current_time, "S ")
+            CheckPointD = snapshots[0].close
+            message = '%s %s %s' % (current_time, "S ", SellPoint, CheckPointD)
+            lineNotifyMessage(token, message)
+            Buystock += 1
+            sellD == 1
+        elif (sellD == 1 and Buystock > 0 and (snapshots[0].close - CheckPointD) > 0 and (snapshots[0].close - CheckPointD) > 5):
+            now = datetime.now()
+            current_time = now.strftime("%y%m%d %H:%M:%S")
+            print("Current Time =", current_time, "B ")
             message = '%s %s %s' % (current_time, "B ", snapshots[0].close)
             lineNotifyMessage(token, message)
-            buyU = 0
-            # order = api.insert_order(
+            Buystock = 0
+            sellD = 0
+
+        print(df['rsi'].tail(1).values, snapshots[0].close)
+        LastTI = time.time()
+
+#order範例
+# order = api.insert_order(
             #     symbol=symbol,
             #     price=close,
             #     action=sj.constant.Action.Buy,
@@ -125,38 +171,3 @@ while True:
             #     quantity=1,
             # )
             # positions.append(order)
-        if sellU == 2 and Buystock == 1:
-            now = datetime.now()
-            current_time = now.strftime("%y%m%d %H:%M:%S")
-            print("Current Time =", current_time, "S ")
-            message = '%s %s %s' % (current_time, "S ", snapshots[0].close)
-            lineNotifyMessage(token, message)
-            Buystock -= 1
-            # order = api.insert_order(
-            #     symbol=symbol,
-            #     price=close,
-            #     action=sj.constant.Action.Sell,
-            #     price_type=sj.constant.TFTOrderPriceType.LMT,
-            #     order_type=sj.constant.TFTOrderType.ROD,
-            #     quantity=1,
-            # )
-            # api.update_status([order])
-        if sellD == 2 and Buystock == 0:
-            now = datetime.now()
-            current_time = now.strftime("%y%m%d %H:%M:%S")
-            print("Current Time =", current_time, "S ")
-            message = '%s %s %s' % (current_time, "S ", snapshots[0].close)
-            lineNotifyMessage(token, message)
-            Buystock += 1
-
-        if buyD == 2 and Buystock == 1:
-            now = datetime.now()
-            current_time = now.strftime("%y%m%d %H:%M:%S")
-            print("Current Time =", current_time, "B ")
-            Buystock -= 1
-            message = '%s %s %s' % (current_time, "B ", snapshots[0].close)
-            lineNotifyMessage(token, message)
-            buyU = 0
-        print(df['K'].tail(1).values, df['D'].tail(1).values, df['J'].tail(1).values, df['Lower'].tail(1).values, df['MA'].tail(1).values,
-              df['Upper'].tail(1).values, df['Close'].tail(1).values, df['MACD'].tail(1).values, df['Signal_Line'].tail(1).values)
-        LastTI = time.time()
--- main.py
+++ main.py
@@ -1,98 +1,173 @@
-import os
-import csv
+import shioaji as sj
+import pandas as pd
+import numpy as np
+import talib
+import requests
 import time
-import math
-import serial
-import mysql.connector
+import csv
+from datetime import datetime, date, timedelta
+from shioaji import TickFOPv1, Exchange
 
-today = time.strftime('%y_%m_%d', time.localtime())
-file = str(today + '_Soiling-data.txt')
-path = '%s\%s'% (r"C:\soft\soiling", file)
-lastTi = 0
+startdate = (date.today() + timedelta(days=-5)).strftime("%Y-%m-%d")
+# print(startdate)
+Buystock = 0
+Sellstock = 0
+sellD = 0
+sellU = 0
+buyD = 0
+buyU = 0
+SellDs = 0
+SellUs = 0
+buyDs = 0
+buyUs = 0
+Openner = 0
+token = '4cwTgoUM0Npnnob1D0aUHCYWxN6huiqVF9poI0G1Efe'
 
-def calc_crc(string):
-    data = bytearray.fromhex(string)
-    crc = 0xFFFF
-    for pos in data:
-        crc ^= pos
-        for i in range(8):
-            if ((crc & 1) != 0):
-                crc >>= 1
-                crc ^= 0xA001
-            else:
-                crc >>= 1
-    result = hex(((crc & 0xff) << 8) + (crc >> 8))
-    result = result[2:]  # 去掉 '0x' 前綴
-    result = result.zfill(4)  # 在前面補齊 0，確保長度為 4
-    return result
 
-mydb = mysql.connector.connect(host="localhost", user="root", password="Oerlikon;1234", port=3306, database="vena_dds_global")
-offsetini = open(r'C:\DDS Data\offset.ini', 'r')
-Line1 = offsetini.readlines()
-dasID = Line1[9].split(':')
-dasID = dasID[1].replace('\n', '')
-offsetini.close
+def lineNotifyMessage(token, msg):
+    headers = {
+        "Authorization": "Bearer " + token,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    payload = {'message': msg}
+    r = requests.post("https://notify-api.line.me/api/notify", headers=headers, params=payload)
+    return r.status_code
 
-SQL = '%s%s%s' % ("SELECT date, soiling FROM ", dasID, ".Daily_soiling order by date desc limit 1")
-cursor = mydb.cursor()
-cursor.execute(SQL)
-data = cursor.fetchall()
-soiling = '%04d' % (math.floor(data[0][1] * 1000))
-with open(path, 'a+') as csvfile:
-    csvfile.write(today)
-    csvfile.write(',')
-    csvfile.write(soiling + '\r\n')
 
-serCOM2 = serial.Serial()
-serCOM2.port = "COM2"
-serCOM2.baudrate = 9600
-serCOM2.bytesize = serial.EIGHTBITS
-serCOM2.parity = serial.PARITY_NONE
-serCOM2.stopbits = serial.STOPBITS_ONE
-serCOM2.timeout = 0.5
-serCOM2.writeTimeout = 0.5
-serCOM2.xonxoff = False
-serCOM2.rtscts = False
-serCOM2.dsrdtr = False
-serCOM2.open()
-with open(r'C:\DDS Data\modbusID.txt') as file:
-    DeviceID = file.readline()
-Readfunc = '03'
-IDfunc = '06001faa'
-year = int(time.strftime('%Y'))
-year = hex(((year & 0xff) << 8) + (year >> 8))
-year = year[2:]
-year = year[2:4] + year[0:2]
-year = year.zfill(4)
-date = int(time.strftime('%m%d'))
-date = hex(((date & 0xff) << 8) + (date >> 8))
-date = date[2:]
-date = date[2:4] + date[0:2]
-date = date.zfill(4)
+today = time.strftime('%y%m', time.localtime())
+pathtoday = str(r'C:\_Phil\Private\stockprice\Log_' + today + '.csv')
+# if os.path.isfile(pathtoday):
+#     pass
+# else:
+#     with open(pathtoday, 'a+', newline='') as csvfile:
+#         writer = csv.writer(csvfile)
+#         writer.writerow(['dateTime', 'Point1', 'Point2', 'difference', 'TWD'])
+api = sj.Shioaji(simulation=True)  # 模擬模式
+api.login(api_key="H6WLFjAN6QaRKucnGBm9TgEGpghBNNhafRyj3xjMLp7M",
+          secret_key="HRgYZfKx7fz2BFYgxxcdxWvtXpCMYPj6BgsqySArEdmF",
+          fetch_contract=False)
+
+api.fetch_contracts(contract_download=True)
+print(api.Contracts.Futures.MXF)
+contracts = [api.Contracts.Futures.MXF.MXFR1]
+today = time.strftime('%Y-%m-%d')
+# print(today)
+#symbol = "TXFF"
+enddate = today
+
+# def quote_callback(exchange:Exchange, tick:TickFOPv1):
+#     dftick = pd.DataFrame({**tick})
+#     dftick.ts = pd.to_datetime(dftick.ts)
+#     dftick.tail(5)
+#     print(dftick)
+
+# api.quote.subscribe(
+#     api.Contracts.Futures.MXF['MXF202307'],
+#     quote_type=sj.constant.QuoteType.Tick,
+#     version=sj.constant.QuoteVersion.v1
+# )
+LastTI = 0
 
 while True:
-    inTi = time.time()
-    if serCOM2.inWaiting() > 0:
-        command = serCOM2.read(10)
-        commdata = command[0:6]
-        inCrc = command[6:10]
-        commandcrc = calc_crc(commdata.hex())
-        if (command[0:1].hex() == DeviceID) & (command[1:5].hex() == IDfunc) & (commandcrc[0:4] == inCrc.hex()):
-            print(time.strftime("%Y-%m-%d %H:%M:%S"), 'Data input: ', {command.hex()})
-            DeviceID = command[5:6].hex()
-            res = command[5:6].hex() + command[1:5].hex() + command[5:6].hex()
-            rescrc = calc_crc(res)
-            resData = bytes.fromhex(res+rescrc)
-            serCOM2.write(resData)
-            print(time.strftime("%Y-%m-%d %H:%M:%S"), 'Data Output: ', {resData.hex()})
-            with open(r'C:\DDS Data\modbusID.txt', 'w') as file:
-                file.write(DeviceID)
-        if (command[0:1].hex() == DeviceID) & (command[1:2].hex() == Readfunc) & (commandcrc[0:4] == inCrc.hex()):
-            print(time.strftime("%Y-%m-%d %H:%M:%S"), 'Data input: ', {command.hex()})
-            rescrc = bytes.fromhex(DeviceID+Readfunc+year+date+soiling)
-            rescrc = calc_crc(rescrc.hex())
-            SendData = bytes.fromhex(DeviceID+Readfunc+'06'+year+date+soiling+rescrc)
-            serCOM2.write(SendData)
-            print(time.strftime("%Y-%m-%d %H:%M:%S"), 'Data Output: ', {SendData.hex()})
-time.sleep(0.001)
+    inTI = time.time()
+    if inTI - LastTI >= 1:
+        # for i in range(0, 12):
+        # price = dftick['amount'].tail(1).values/dftick['volume'].tail(1).values
+        # print(price)
+        snapshots = api.snapshots(contracts)
+        kbars = api.kbars(
+            contract=api.Contracts.Futures.MXF.MXFR1,
+            start=startdate, end=enddate
+        )
+        # 將Tick數據轉換為DataFrame
+        df = pd.DataFrame({**kbars})
+        df.ts = pd.to_datetime(df.ts)
+        df.head(20)
+        df['rsi'] = talib.RSI(df['Close'], timeperiod=14)
+        lastRSI = df['rsi'].tail(1).values[-1]
+        if (lastRSI < 20.0):
+            if (lastRSI > 20.0):
+                buyU = 1
+            else:
+                buyU = 0
+        else:
+            buyU = 0
 
+        if (lastRSI > 80.0):
+            if (lastRSI < 80.0):
+                sellD = 1
+            else:
+                sellD = 0
+        else:
+            sellD = 0
+
+        if buyU == 1 and Buystock == 0:
+            now = datetime.now()
+            current_time = now.strftime("%y%m%d %H:%M:%S")
+            print("Current Time =", current_time, "B ")
+            BuyPoint = snapshots[0].close
+            CheckPointB = snapshots[0].close
+            Buystock += 1
+            buyU == 1
+            message = '%s %s %s' % (current_time, "B ", BuyPoint)
+            lineNotifyMessage(token, message)
+        elif sellD == 1 and Buystock == 0:
+            now = datetime.now()
+            current_time = now.strftime("%y%m%d %H:%M:%S")
+            print("Current Time =", current_time, "S ")
+            SellPoint = snapshots[0].close
+            CheckPointD = snapshots[0].close
+            message = '%s %s %s' % (current_time, "S ", SellPoint)
+            lineNotifyMessage(token, message)
+            Buystock += 1
+            sellD == 1
+
+        if (buyU == 1 and Buystock != 0 and (snapshots[0].close - CheckPointB) >= 5):
+            now = datetime.now()
+            current_time = now.strftime("%y%m%d %H:%M:%S")
+            print("Current Time =", current_time, "B ")
+            CheckPointB = snapshots[0].close
+            Buystock += 1
+            message = '%s %s %s' % (current_time, "B ", BuyPoint, CheckPointB)
+            lineNotifyMessage(token, message)
+            buyU = 1
+        elif (buyU == 1 and Buystock > 0 and (CheckPointB - snapshots[0].close) > 0 and (CheckPointB - snapshots[0].close) < 5):
+            now = datetime.now()
+            current_time = now.strftime("%y%m%d %H:%M:%S")
+            print("Current Time =", current_time, "S ")
+            earn =
+            message = '%s %s %s' % (current_time, "S ", snapshots[0].close)
+            buyU = 0
+            lineNotifyMessage(token, message)
+            Buystock = 0
+        if (sellD == 1 and Buystock != 0 and (CheckPointD - snapshots[0].close) > 5):
+            now = datetime.now()
+            current_time = now.strftime("%y%m%d %H:%M:%S")
+            print("Current Time =", current_time, "S ")
+            CheckPointD = snapshots[0].close
+            message = '%s %s %s' % (current_time, "S ", SellPoint, CheckPointD)
+            lineNotifyMessage(token, message)
+            Buystock += 1
+            sellD == 1
+        elif (sellD == 1 and Buystock > 0 and (snapshots[0].close - CheckPointD) > 0 and (snapshots[0].close - CheckPointD) > 5):
+            now = datetime.now()
+            current_time = now.strftime("%y%m%d %H:%M:%S")
+            print("Current Time =", current_time, "B ")
+            message = '%s %s %s' % (current_time, "B ", snapshots[0].close)
+            lineNotifyMessage(token, message)
+            Buystock = 0
+            sellD = 0
+
+        print(df['rsi'].tail(1).values, snapshots[0].close)
+        LastTI = time.time()
+
+#order範例
+# order = api.insert_order(
+            #     symbol=symbol,
+            #     price=close,
+            #     action=sj.constant.Action.Buy,
+            #     price_type=sj.constant.TFTOrderPriceType.LMT,
+            #     order_type=sj.constant.TFTOrderType.ROD,
+            #     quantity=1,
+            # )
+            # positions.append(order)
